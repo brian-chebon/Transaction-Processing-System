f2dc94342a6b9c49eb96a2d2b80c5c5c9b33f236
Brian Chebon <brianlchebon@gmail.com>
2025-01-27 14:31:40 +0300

    chore: fix routes and serve on web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Include API routes
+require base_path('routes/api.php');
+
+// Default home route
 Route::get('/', function () {
     return view('welcome');
 });
